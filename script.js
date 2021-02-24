c56aab213ccded2a0b785288a114e89fb658dfd3
McCall Woodward <woodwardem@g.crookcoountyschools.org>
2021-02-23 20:31:40 -0800

    squared every number in array for lab 52

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-let foods = ['pie', 'steak', 'apple', 'bread', 'bacon', 'cheese', 'rice', 'orange', 'waffle', 'jam'];
-console.log(foods[0]);
-console.log(foods[9]);
-function firstLast(foods) {
-    let firstOne = foods[0];
-    let lastOne = foods[9];
-    let outPut = {};
-    outPut[firstOne]=lastOne
-    return outPut;
-}
-let display = firstLast(foods);
-console.log(display);
+//let foods = ['pie', 'steak', 'apple', 'bread', 'bacon', 'cheese', 'rice', 'orange', 'waffle', 'jam'];
+//console.log(foods[0]);
+//console.log(foods[9]);
+//function firstLast(foods) {
+   // let firstOne = foods[0];
+   // let lastOne = foods[9];
+  //  let outPut = {};
+  //  outPut[firstOne]=lastOne
+  //  return outPut;
+//}
+//let display = firstLast(foods);
+//console.log(display);
 
 
-let numbers = ['2', '4', '3', '6','5'];
-console.log(numbers[1 + 2]);
-console.log(numbers[[0] * [1] * [4]]);
+//let numbers = ['2', '4', '3', '6','5'];
+//console.log(numbers[1 + 2]);
+//console.log(numbers[[0] * [1] * [4]]);
+
+
---
+++ working-with-array.js
@@ -0,0 +1,3 @@
+let nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+for (let i = 0; i++;);
+console.log(nums.map(v => Math.pow(v, 2)));
